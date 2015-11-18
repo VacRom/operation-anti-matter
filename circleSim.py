2b34a527614eb09e244a0eadeaef441944a249eb
Tommy Jun <tbj2cu@virginia.edu>
2015-11-18 12:11:58 -0500

    Optimized runtime. Need to add AI

--- circleSim.py
+++ circleSim.py
@@ -4,14 +4,15 @@ import math
 
 pg.init()
 
-width = 600
+width = 1200
 height = 600
 size = (width, height)
 screen = pg.display.set_mode(size)
 pg.display.set_caption("Circles Simulation")
-totalCircles = 500
+totalCircles = 600
 
 state = []
+y = 0
 
 for n in range(totalCircles):
     x = rn.randint(0, 3)
@@ -23,7 +24,11 @@ for n in range(totalCircles):
         color = (0, 0, 255)
     if x == 3:
         color = (0, 0, 0)
-    state.append([rn.randint(0, width), rn.randint(-10,10), rn.randint(0, height), rn.randint(-10, 10), rn.randint(5, 10), color])
+    if x == 0 or 1:
+        y = rn.randint(1, 30)
+    if x == 2 or 3:
+        y = - rn.randint(1, 30)
+    state.append([rn.randint(0, width), y, rn.randint(0, height), y, rn.randint(2, 5), color])
 
 done = False
 
@@ -36,31 +41,32 @@ while not done:
 
     # Clears the screen
     screen.fill((255, 255, 255))
-
+    
     # Draw circles and apply velocity.
     for n in range(totalCircles):
         if state[n][4] != 0:
             pg.draw.circle(screen, state[n][5], (state[n][0], state[n][2]), state[n][4], 0)
-        state[n][0] = state[n][0]+state[n][1]
-        state[n][2] = state[n][2]+state[n][3]
-        # Boundaries
-        if state[n][0] < state[n][4] or state[n][0] > width-state[n][4] and state[n][4] != 0:
-            state[n][1] = - state[n][1]
             state[n][0] = state[n][0]+state[n][1]
-        if state[n][2] < state[n][4] or state[n][2] > height-state[n][4] and state[n][4] != 0:
-            state[n][3] = - state[n][3]
             state[n][2] = state[n][2]+state[n][3]
-            # Collisions
-        for m in range(totalCircles):
-            if n != m and math.hypot(state[m][0]-state[n][0],state[m][2]-state[n][2]) < state[n][4]+state[m][4] and (state[n][4] != 0 or state[m][4] != 0):
-                if state[n][4] == state[m][4]:
-                    state[n][1] = - state[n][1]
-                    state[n][3] = - state[n][3]
-                    state[m][1] = - state[m][1]
-                    state[m][3] = - state[m][3]
-                if state[n][4] > state[m][4]:
-                    state[n][4] = int(math.sqrt(((state[n][4]) * (state[n][4])) + ((state[m][4] * state[m][4]))))
-                    state[m][4] = 0
+            # Boundaries
+            if state[n][0] < state[n][4] or state[n][0] > width-state[n][4]:
+                state[n][0] = state[n][0]-state[n][1]
+                state[n][1] = - state[n][1]
+            if state[n][2] < state[n][4] or state[n][2] > height-state[n][4]:
+                state[n][2] = state[n][2]-state[n][3]
+                state[n][3] = - state[n][3]
+             # Collisions
+            for m in range(totalCircles):
+                if state[m][4] != 0:
+                    if n != m and math.hypot(state[m][0]-state[n][0],state[m][2]-state[n][2]) < state[n][4]+state[m][4] and state[m][4] != 0:
+                        if state[n][4] == state[m][4]:
+                            state[n][1] = - state[n][1]
+                            state[n][3] = - state[n][3]
+                            state[m][1] = - state[m][1]
+                            state[m][3] = - state[m][3]
+                        if state[n][4] > state[m][4]:
+                            state[n][4] = int(math.sqrt(((state[n][4]) * (state[n][4])) + ((state[m][4] * state[m][4]))))
+                            state[m][4] = 0
 
     pg.display.flip()
     clock.tick(60)
