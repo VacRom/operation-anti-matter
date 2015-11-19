d67941c981ad429410b60957b2b37f2c789eed84
Tommy Jun <tbj2cu@virginia.edu>
2015-11-18 22:26:47 -0500

    AIs made: Black, Red, Green

--- circleSim.py
+++ circleSim.py
@@ -9,13 +9,15 @@ height = 600
 size = (width, height)
 screen = pg.display.set_mode(size)
 pg.display.set_caption("Circles Simulation")
-totalCircles = 600
+totalCircles = 100
 
 state = []
-y = 0
 
 for n in range(totalCircles):
+    radius = rn.randint(5,10)
     x = rn.randint(0, 3)
+    velx = rn.randint(-7,7)
+    vely = rn.randint(-7,7)
     if x == 0:
         color = (255, 0, 0)
     if x == 1:
@@ -24,14 +26,10 @@ for n in range(totalCircles):
         color = (0, 0, 255)
     if x == 3:
         color = (0, 0, 0)
-    if x == 0 or 1:
-        y = rn.randint(1, 30)
-    if x == 2 or 3:
-        y = - rn.randint(1, 30)
-    state.append([rn.randint(0, width), y, rn.randint(0, height), y, rn.randint(2, 5), color])
+    state.append([rn.randint(0, width), velx, rn.randint(0, height), vely, radius, color])
 
+# Setting up the game.
 done = False
-
 clock = pg.time.Clock()
 
 while not done:
@@ -39,15 +37,44 @@ while not done:
         if event.type == pg.QUIT:
             done = True
 
-    # Clears the screen
+    # Clears the screen.
     screen.fill((255, 255, 255))
-    
+    # Clears the lists for calculating the centroid.
+    stateGx = []
+    stateGy = []
     # Draw circles and apply velocity.
     for n in range(totalCircles):
         if state[n][4] != 0:
             pg.draw.circle(screen, state[n][5], (state[n][0], state[n][2]), state[n][4], 0)
-            state[n][0] = state[n][0]+state[n][1]
-            state[n][2] = state[n][2]+state[n][3]
+         # AI
+            # Black - Constant Speed.
+            # Red - Random Walk.
+            if state[n][5] == (255,0,0):
+                state[n][1] = rn.randint(-15, 15)
+                state[n][3] = rn.randint(-15, 15)
+            # Green - Walk towards the center of all green points.
+            if state[n][5] == (0,255,0):
+                stateGx = []
+                stateGy = []
+                for m in range(totalCircles):
+                    if state[m][5] == (0,255,0):
+                        stateGx.append(state[m][0])
+                        stateGy.append(state[m][2])
+                c = [stateGx,stateGy]
+                centroid = (sum(c[0])/len(c[0]),sum(c[1])/len(c[1]))
+                if state[n][0] < centroid[0]:
+                    state[n][0] = state[n][0] + rn.randint(3,7)
+                if state[n][0] > centroid[0]:
+                    state[n][0] = state[n][0] - rn.randint(1,3)
+                if state[n][2] < centroid[1]:
+                    state[n][2] = state[n][2] + rn.randint(1,3)
+                if state[n][2] > centroid[1]:
+                    state[n][2] = state[n][2] - rn.randint(1,3)
+            # Blue
+            # Apply velocity
+            if state[n][5] != (0,255,0):
+                state[n][0] = state[n][0]+state[n][1]
+                state[n][2] = state[n][2]+state[n][3]
             # Boundaries
             if state[n][0] < state[n][4] or state[n][0] > width-state[n][4]:
                 state[n][0] = state[n][0]-state[n][1]
@@ -68,6 +95,11 @@ while not done:
                             state[n][4] = int(math.sqrt(((state[n][4]) * (state[n][4])) + ((state[m][4] * state[m][4]))))
                             state[m][4] = 0
 
+    for m in range(totalCircles):
+        if (0,255,0) in state[m] and state[m][4] != 0:
+            pg.draw.circle(screen, (0, 100, 0), (int(centroid[0]),int(centroid[1])), 10, 0)
+
     pg.display.flip()
-    clock.tick(60)
+    clock.tick(30)
 pg.quit()
+
--- notes.py
+++ notes.py
@@ -1,26 +1,2 @@
-import random as rn
-width = 1200
-height = 600
-
-state = []
-
-totalCircles = 10
-for n in range(totalCircles):
-    x = rn.randint(0, 3)
-    if x == 0:
-        color = (255, 0, 0)
-    if x == 1:
-        color = (0, 255, 0)
-    if x == 2:
-        color = (0, 0, 255)
-    if x == 3:
-        color = (0, 0, 0)
-    state.append([rn.randint(0, width), rn.randint(-10,10), rn.randint(0, height), rn.randint(-10, 10), rn.randint(5, 10), color])
-
-
-for n in range(totalCircles):
-    print(state)
-    def zeroRad(state):
-        if state[n][4] > 0:
-            return state[n]
-    filter(zeroRad, state)
+c = [[1,4,-5],[3,-2,9]] # of the form [[x1,x2,x3],[y1,y2,y3]]
+centroide = (sum(c[0])/len(c[0]),sum(c[1])/len(c[1]))
